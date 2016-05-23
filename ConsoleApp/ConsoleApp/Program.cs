2ab713ae54aa78c63f02cd461feaf3b6eb8a6614
laurentiub <laurentiu.burdusel@gmail.com>
2016-05-23 22:28:33 +0300

    ClientApp added

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClient = new Client();
+
+            var duplexProxy = new DuplexChannelFactory<IServer>(myClient, new NetNamedPipeBinding(), "net.pipe://localhost/test");
+            var duplexConnection =  duplexProxy.CreateChannel();
+            duplexConnection.Connect();
+
+            Console.ReadKey();            
+        }
+    }
+
+    [ServiceContract(CallbackContract = typeof(IClient))]
+    public interface IServer
+    {
+        [OperationContract]
+        void Connect();
+
+        [OperationContract]
+        bool Test();
+    }
+
+    [ServiceContract]
+    public interface IClient
+    {
+        [OperationContract]
+        bool Response(int x);
+    }
+
+    public class Client : IClient
+    {
+        public bool Response(int x)
+        {
+            Console.WriteLine("Response method called");
+            return (x % 2 == 0);
+        }
+    }
+
+}
